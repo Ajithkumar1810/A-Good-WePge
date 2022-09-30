444ebd5cb79a1945e17b921d39459512cb3ec879
Ajithkumar1810 <jaganajith18@gmail.com>
2022-09-30 16:01:43 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppoinmentDetailsComponent } from './appoinment-details/appoinment-details.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Appoinment_Details',component:AppoinmentDetailsComponent},
+  {path:'',component:MenubarComponent},
+  // {path:'',component:},
+  // {path:'',component:},
+  // {path:'',component:},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppoinmentDetailsComponent } from './appoinment-details/appoinment-details.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { TargetPageComponent } from './target-page/target-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppoinmentDetailsComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MenubarComponent,
+    TargetPageComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
